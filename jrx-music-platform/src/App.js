18e105b7697b26752e550d65132096aca9b5ade3
Jarax <jaraxkapila73@gmail.com>
2022-08-19 23:03:00 +0100

    Render recently played on load

--- jrx-music-platform/src/App.js
+++ jrx-music-platform/src/App.js
@@ -1,135 +1,223 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Login from './components/Login';
 import Menu from './components/Menu';
 import Player from './components/Player';
 import Aside from './components/Aside';
-import {RiSearch2Line} from 'react-icons/ri';
-
+import { RiSearch2Line } from 'react-icons/ri';
+import { RiPlayCircleFill } from 'react-icons/ri';
 
 export default function App() {
-  const clientID = "af6fe4b7a75e4651bd1531de3f541e53";
-    const redirectUrl = "http://localhost:3000";
-    const apiUrl = "https://accounts.spotify.com/authorize";
-    const responseType = "token";
+    const clientID = 'af6fe4b7a75e4651bd1531de3f541e53';
+    const redirectUrl = 'http://localhost:3000';
+    const apiUrl = 'https://accounts.spotify.com/authorize';
+    const responseType = 'token';
     const scope = [
-        "streaming",
-        "user-read-email", 
-        "user-read-private", 
-        "user-modify-playback-state",
-        "user-read-playback-state", 
-        "user-read-currently-playing",
-        "user-read-recently-played",
-        "user-read-playback-position",
-        "user-top-read",
-        "user-library-read",
-        "user-library-modify"
+        'streaming',
+        'user-read-email',
+        'user-read-private',
+        'user-modify-playback-state',
+        'user-read-playback-state',
+        'user-read-currently-playing',
+        'user-read-recently-played',
+        'user-read-playback-position',
+        'user-top-read',
+        'user-library-read',
+        'user-library-modify',
     ];
-    const [token, setToken] = useState ("");
-    const [searchKey, setSearchKey] = useState ("");
-    const [artists, setArtists] = useState ("");
-    const [url, setUrl] = useState ("");
-    
+    const [token, setToken] = useState('');
+    const [searchKey, setSearchKey] = useState('');
+    const [artists, setArtists] = useState('');
+    const [url, setUrl] = useState('');
+
     useEffect(() => {
         const hash = window.location.hash;
-        let token = window.localStorage.getItem ("token");
+        let token = window.localStorage.getItem('token');
 
-        if(!token && hash){
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
-            window.location.hash = "";
-            window.localStorage.setItem ("token", token);
-            
+        if (!token && hash) {
+            token = hash
+                .substring(1)
+                .split('&')
+                .find((elem) => elem.startsWith('access_token'))
+                .split('=')[1];
+            window.location.hash = '';
+            window.localStorage.setItem('token', token);
+
             console.log(localStorage);
         }
         setToken(token);
-    }, []);
+        console.log(url);
 
+        window.onload = axios
+            .get(
+                'https://api.spotify.com/v1/me/player/recently-played?limit=6',
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            )
+            .then((response) => setUrl(response.data.items));
+    }, []);
     const handleClick = () => {
-        window.location.href = `${apiUrl}?client_id=${clientID}&redirect_uri=${redirectUrl}&scope=${scope.join(" ")}&response_type=${responseType}&show_dialog=true`;
-        };
-        
+        window.location.href = `${apiUrl}?client_id=${clientID}&redirect_uri=${redirectUrl}&scope=${scope.join(
+            '%20'
+        )}&response_type=${responseType}&show_dialog=true`;
+    };
+
     const logout = () => {
         setToken(null);
-        window.localStorage.removeItem("token");
-    }
+        window.localStorage.removeItem('token');
+    };
 
-    
+    // console.log(response.items[0].track);
+    // setUrl(data.items[0].track.uri);
+    // setUrl(response.items.map((item) => item.track));
+
     const searchArtists = async (e) => {
         e.preventDefault();
 
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+        const { data } = await axios.get('https://api.spotify.com/v1/search', {
             headers: {
-                Authorization: `Bearer ${token}`
-            }, 
+                Authorization: `Bearer ${token}`,
+            },
             params: {
                 q: searchKey,
-                type: "artist"
-            }
-        })
-        
+                type: 'artist',
+            },
+        });
+
         setArtists(data.artists.items);
         console.log(data.artists.items);
-    }
-    // const searchTracks = async (e) => {
-    //     e.preventDefault();
-
-    //     const {data} = await  axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=10', {
-    //         headers: {
-    //             Accept: "application/json",
-    //             'Content-type': "application/json",
-    //             Authorization: `Bearer ${token}`
-    //         }
-    //     })
-    //     console.log(data.items);
-    //     setUrl(data);
-    //   }
-
+    };
     console.log(artists);
     const renderArtists = () => {
-        return artists === "" ? null : artists.map(artist => (
-            <div className="artist" key={artist.id}>
-                {artist.images.length ? <img width={"100%"} height={"100%"} src={artist.images[0].url} alt="" /> : <div>No image</div>}
-                <div className="artist-detail">
-                    {artist.name}<br/>
-                    {artist.genres[0]}
-                </div>
-            </div>
-            )
-        )
-    }
+        return artists === ''
+            ? null
+            : artists.map((artist) => (
+                  <div className="artist" key={artist.id}>
+                      {artist.images.length ? (
+                          <img
+                              width={'100%'}
+                              height={'100%'}
+                              src={artist.images[0].url}
+                              alt=""
+                          />
+                      ) : (
+                          <div>No image</div>
+                      )}
+                      <div className="artist-detail">
+                          {artist.name}
+                          <br />
+                          {artist.genres[0]}
+                      </div>
+                  </div>
+              ));
+    };
+    const renderRecentPlayed = () => {
+        return url ? (
+            url
+                .filter(function (ele, pos) {
+                    return url.indexOf(ele) === pos;
+                })
+                .map((item) => (
+                    <div
+                        className="artist"
+                        key={item.track.id}
+                        style={{
+                            width: '20%',
+                            position: 'relative',
+                        }}
+                    >
+                        <div>
+                            <img
+                                src={item.track.album.images[0].url}
+                                alt=""
+                                style={{ height: '80%' }}
+                            />
+                        </div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                //   flexDirection: 'column',
+                                padding: '5px',
+                            }}
+                        >
+                            <div>
+                                <span style={{ color: 'white' }}>
+                                    {item.track.artists[0].name} <br />
+                                </span>
+                                <span style={{ color: '#CFC5C5' }}>
+                                    {item.track.name}
+                                </span>
+                            </div>
+                            {/* <div
+                              style={{
+                                  alignSelf: 'flex-end',
+                                  justifySelf: 'flex-end',
+                              }}
+                          >
+                              <RiPlayCircleFill
+                                  className="notify"
+                                  onclick={() => setUrl(item.track.uri)}
+                              />
+                          </div> */}
+                        </div>
+                        {console.log(url[0].track.album.images[0].url)}
+                        {console.log(
+                            url.map((item) => item.track.album.images[0].url)
+                        )}
+                    </div>
+                ))
+        ) : (
+            <p style={{ justifySelf: 'center', alignSelf: 'center' }}>
+                Chargement
+            </p>
+        );
+    };
 
-  return (
-    <div className="container">
-        {!token ?
-        <div className="home">
-            <div className="title">
-            <h1>Welcome to Jrx Music by   </h1>               
-            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="spotify" />
-            </div>
-            <button onClick={handleClick}>Connect Spotify</button>
-            <h2>Please login</h2> 
+    return (
+        <div className="container">
+            {!token ? (
+                <Login handleClick={handleClick} />
+            ) : (
+                <React.Fragment>
+                    <Menu logout={logout} />
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <div className="section">
+                            <div className="row">
+                                <form
+                                    className="search"
+                                    onSubmit={searchArtists}
+                                >
+                                    <button
+                                        className="searchArtists"
+                                        type="submit"
+                                    >
+                                        <input
+                                            type="text"
+                                            onChange={(e) =>
+                                                setSearchKey(e.target.value)
+                                            }
+                                        />
+                                        <RiSearch2Line
+                                            style={{ color: '#05476b' }}
+                                        />
+                                    </button>
+                                </form>
+                            </div>
+                            <div className="render-artists">
+                                {renderArtists()}
+                                {renderRecentPlayed()}
+                            </div>
+                        </div>
+                        <Player token={token} />
+                    </div>
+                    <Aside token={token} redirectUrl={redirectUrl} />
+                </React.Fragment>
+            )}
         </div>
-            : <React.Fragment>
-          <Menu logout={logout}/>
-          <div style={{display: 'flex', flexDirection: 'column'}}>
-          <div className="section">
-            <div className="row">           
-              <form className="search" onSubmit={searchArtists}>                  
-                  <button className="searchArtists" type="submit">
-                    <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-                    <RiSearch2Line style={{color: '#05476b'}} />
-                  </button>
-              </form>
-            </div>
-            <div className="render-artists">
-              {renderArtists() }
-            </div>
-            
-          </div>
-                <Player token={token} />
-            </div>
-            <Aside token={token} redirectUrl={redirectUrl}/>
-            </React.Fragment>
-        }
-    </div>
-  )
+    );
 }
--- jrx-music-platform/src/components/Player.js
+++ jrx-music-platform/src/components/Player.js
@@ -1,59 +1,58 @@
-import { WebPlaybackSDK } from "react-spotify-web-playback-sdk";
-import React, { useState, useEffect } from "react";
-import SpotifyPlayer from "react-spotify-web-playback";
-import axios from "axios";
+import { WebPlaybackSDK } from 'react-spotify-web-playback-sdk';
+import React, { useState, useEffect } from 'react';
+import SpotifyPlayer from 'react-spotify-web-playback';
+import axios from 'axios';
 
-const Player = ({ token}) => {
-  const [url, setUrl] = useState("");
+const Player = ({ token }) => {
+    const [url, setUrl] = useState('');
+    // const url = 'spotify:artist:5pKCCKE2ajJHZ9KAiaK11H';
+    const [play, setPlay] = useState(false);
 
-    console.log(token);
-  const [play, setPlay] = useState(false);
-  
-  const initialVolume = 20;
+    const initialVolume = 20;
 
-  useEffect(() => {
-    setPlay(true)
-    window.onload = searchTracks();
-  }, [url])
+    const searchTracks = async () => {
+        const { data } = await axios.get(
+            'https://api.spotify.com/v1/me/player/recently-played?',
+            {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        console.log(data.items[0].track);
+        // setUrl(data.items[0].track.uri);
+        setUrl(data.items.map((item) => item.track));
+    };
+    console.log(url);
+    useEffect(() => {
+        setPlay(true);
+        // searchTracks();
+    }, [url]);
 
-  console.log(url);
+    if (!token) return null;
+    return (
+        <div className="player-container">
+            <SpotifyPlayer
+                token={token}
+                showSaveIcon
+                callback={(state) => !state.isPlaying && setPlay(false)}
+                initialVolume={initialVolume}
+                play={play}
+                uris={url ? url : []}
+                styles={{
+                    activeColor: '#fff',
+                    bgColor: '#05476b',
+                    color: '#fff',
+                    loaderColor: '#fff',
+                    sliderColor: '#515151',
+                    trackArtistColor: '#CFC5C5',
+                    trackNameColor: '#fff',
+                }}
+            />
+        </div>
+    );
+};
 
-  const searchTracks = async () => {
-  
-    const {data} = await axios.get('https://api.spotify.com/v1/me/player/recently-played?', {
-        headers: {
-            Accept: "application/json",
-            'Content-type': "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
-    console.log(data.items);
-    setUrl(data.items.map(item=> item.track.uri));
-  }
-  
-
-  if (!token) return null
-  return (
-    <div className="player-container">
-      <SpotifyPlayer
-        token={token}
-        showSaveIcon
-        callback={state => !state.isPlaying && setPlay(false)}
-        initialVolume={initialVolume}
-        play={play}
-        uris={url ? url : []}
-        styles={{
-          activeColor: "#fff",
-          bgColor: "#05476b",
-          color: "#fff",
-          loaderColor: "#fff",
-          sliderColor: "#515151",
-          trackArtistColor: "#CFC5C5",
-          trackNameColor: "#fff",
-        }}
-      />
-    </div>
-  )
-}
-
-export default Player
+export default Player;
