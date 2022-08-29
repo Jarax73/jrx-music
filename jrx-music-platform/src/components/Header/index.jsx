6e2f0f0472d1593adb867700a8761016a9b61918
Jarax <jaraxkapila73@gmail.com>
2022-08-29 08:25:39 +0100

    New build folder

--- jrx-music-platform/src/components/Header/index.jsx
+++ jrx-music-platform/src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { RiHome2Fill } from 'react-icons/ri';
 import { RiSearch2Line } from 'react-icons/ri';
 import { RiPlayList2Fill } from 'react-icons/ri';
@@ -7,52 +8,56 @@ import { RiLogoutCircleRFill } from 'react-icons/ri';
 import Profile from '../Profile';
 
 export default function Menu({ logout, profile }) {
+    Menu.propTypes = {
+        logout: PropTypes.bool,
+        profile: PropTypes.object,
+    };
     return (
-            <div className="menu">
-                <p className="logo">Jrx Music</p>
-                <ul>
+        <div className="menu">
+            <p className="logo">Jrx Music</p>
+            <ul>
                 <li>
-                    <Profile profile={profile}/>
+                    <Profile profile={profile} />
                 </li>
-                    <Link to="/">
-                        <li>
-                            <span className="icons">
-                                <RiHome2Fill />
-                            </span>
-                            <div>Home</div>
-                        </li>
-                    </Link>
-                    <Link to="/search">
-                        <li>
-                            <span className="icons">
-                                <RiSearch2Line />
-                            </span>
-                            <div>Search</div>
-                        </li>
-                    </Link>
-                    <Link to="/library">
-                        <li>
-                            <span className="icons">
-                                <RiMusic2Fill />
-                            </span>
-                            <div>Library</div>
-                        </li>
-                    </Link>
-                    <Link to="/playlists">
-                        <li>
-                            <span className="icons">
-                                <RiPlayList2Fill />
-                            </span>
-                            <div>Playlists</div>
-                        </li>
-                    </Link>
-                </ul>
-                <div className="logout" onClick={logout}>
-                    <span className="icons">
-                        <RiLogoutCircleRFill />
-                    </span>
-                    <span>Log Out </span>
-                </div>
+                <Link to="/">
+                    <li>
+                        <span className="icons">
+                            <RiHome2Fill />
+                        </span>
+                        <div>Home</div>
+                    </li>
+                </Link>
+                <Link to="/search">
+                    <li>
+                        <span className="icons">
+                            <RiSearch2Line />
+                        </span>
+                        <div>Search</div>
+                    </li>
+                </Link>
+                <Link to="/library">
+                    <li>
+                        <span className="icons">
+                            <RiMusic2Fill />
+                        </span>
+                        <div>Library</div>
+                    </li>
+                </Link>
+                <Link to="/playlists">
+                    <li>
+                        <span className="icons">
+                            <RiPlayList2Fill />
+                        </span>
+                        <div>Playlists</div>
+                    </li>
+                </Link>
+            </ul>
+            <div className="logout" onClick={logout}>
+                <span className="icons">
+                    <RiLogoutCircleRFill />
+                </span>
+                <span>Log Out </span>
             </div>
+        </div>
     );
 }
